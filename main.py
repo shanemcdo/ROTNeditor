1be0da68a84b72e5a0c6ec9b99d542aaded7a005
Shane McDonough <shanemcdman@gmail.com>
2021-12-18 19:12:42 -0800

    Update for mac

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import subprocess
+EDITOR = os.environ['EDITOR']
 
-def parse_args(args: list) -> ("relative_path", "gvim_flags", int):
+def parse_args(args: list) -> ("relative_path", "vim_flags", int):
     """ converts args into a more usable format
-    :returns: a tuple of the relative path name and the flags for gvim and the number of rotations to encode
+    :returns: a tuple of the relative path name and the flags for vim and the number of rotations to encode
     """
     _ = args.pop(0)
     if len(args) < 1:
@@ -12,23 +15,23 @@ def parse_args(args: list) -> ("relative_path", "gvim_flags", int):
     elif len(args) == 1:
         return args, "", 1
     relative_paths = []
-    gvim_flags = ""
+    vim_flags = ""
     n = int(args.pop(0))
     for arg in args:
         if arg.startswith('-'):
-            gvim_flags += arg + " "
+            vim_flags += arg + " "
         else:
             relative_paths.append(arg)
     length = len(relative_paths)
     if length < 1:
         raise(Exception("Too few arguments"))
-    return relative_paths, gvim_flags, n
+    return relative_paths, vim_flags, n
 
-def open_files(relative_paths, gvim_flags, n) -> None:
+def open_files(relative_paths, vim_flags, n) -> None:
     for path in relative_paths:
         if n != 0 and os.path.exists(path):
             os.system("ROTN " + str(-n) + " -f " + path + " -o " + path)
-    p = subprocess.Popen(("gvim " + ' '.join(relative_paths) + " " + gvim_flags).split())
+    p = subprocess.Popen((EDITOR + ' ' + ' '.join(relative_paths) + " " + vim_flags).split())
     p.wait()
     if n != 0:
         for path in relative_paths:
@@ -37,14 +40,14 @@ def open_files(relative_paths, gvim_flags, n) -> None:
 def print_command_template() -> None:
     print(
             "Usage of command:\n" +
-            "\tROTNe {n; spaces to rotate} {file(s)} [gvim flags]\n" +
+            "\tROTNe {n; spaces to rotate} {file(s)} [ flags]\n" +
             "\t\tsee vim --help for info on flags\n"
             )
 
 def main():
     try:
-        paths, gvim_flags, n = parse_args(sys.argv)
-        open_files(paths, gvim_flags, n)
+        paths, vim_flags, n = parse_args(sys.argv)
+        open_files(paths, vim_flags, n)
     except Exception as e:
         print("Error:", e)
         print_command_template()
